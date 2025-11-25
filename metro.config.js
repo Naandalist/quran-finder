5946abde85ccbddcb16d4960596146b149372e00
Naandalist <listiananda.apriliawan@gmail.com>
2025-11-25 22:51:56 +0700

    feat: integrate rozenite

--- metro.config.js
+++ metro.config.js
@@ -1,3 +1,4 @@
+const { withRozenite } = require('@rozenite/metro');
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
 /**
@@ -8,4 +9,4 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  */
 const config = {};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = withRozenite(mergeConfig(getDefaultConfig(__dirname), config), { enabled: process.env.WITH_ROZENITE === 'true' });
